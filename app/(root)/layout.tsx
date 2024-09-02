24c5a4a4d83dc106fbdbfd75bbd15dd91ac4c631
NOBLE ESELASE VULLEY <nevulley@st.ug.edu.gh>
2024-09-02 16:23:00 +0000

    Implement the Complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
-
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from 'next/image'
 
 
 export default function RootLayout({
@@ -6,10 +8,29 @@ export default function RootLayout({
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+
+    const loggedIn = {firstName: 'Nobleson', lastName: 'Eselase'};
+
+
     return (
-       <main>
-           SIDEBAR
-           {children}
+       <main className="flex h-screen w-full font-inter">
+           <Sidebar user={loggedIn}/>
+           <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image 
+                        src = "/icons/logo.svg" 
+                        width={30}
+                        height={30}
+                        alt="logo"
+                    />
+                    <div>
+                        <MobileNav user={loggedIn}/>
+                    </div>
+                </div>
+                {children}
+           </div>
+           
        </main>
     );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,40 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 
 const Home = () => {
 
+    const loggedIn = {firstName: 'Fiifi', lastName: 'Nobleson', email:'fiifinobleson@gmail.com'};
+
     return (
-        <div>
-            Home
-        </div>
+        <section className = "home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                    type = "greeting"
+                    title = "Welcome"
+                    user = {loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your accounts and transactions efficiently."
+                    />
+
+                         <TotalBalanceBox
+                    accounts = {[]}
+                    totalBanks = {1}
+                    totalCurrentBalance = {130250.67}
+                />
+                </header>
+
+                RECENT TRANSACTIONS
+
+           
+            </div>
+            <RightSidebar 
+            user={loggedIn}
+            transactions = {[]}
+            banks={[{currentBalance: 130250.67}, {currentBalance: 205550.67}]}
+            />
+        </section>
     )
 }
 export default Home;
